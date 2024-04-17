6efcdad573365402dce5058edf44386426a68cce
Stefan Izota <1038308+stefanizota@users.noreply.github.com>
2024-04-17 14:56:55 +0300

    Change dependency oceanprotocol/lib to forked.

--- app.config.js
+++ app.config.js
@@ -11,12 +11,10 @@ module.exports = {
 
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
-  // chainIds: [1, 137, 10],
   chainIds: [842],
 
   // List of all supported chainIds. Used to populate the Chains user preferences list.
   chainIdsSupported: [842],
-  // chainIdsSupported: [1, 137, 10, 80001, 11155111, 842],
 
   customProviderUrl: process.env.NEXT_PUBLIC_PROVIDER_URL,
 
